ca69992f15c4e0d400811df252e1b24f66054b0f
jwbuurlage <jwbuurlage@me.com>
2014-03-02 02:59:15 +0100

    updated

---
+++ scripts/config.def.h
@@ -0,0 +1,36 @@
+/* The height of the bar (in pixels) */
+#define BAR_HEIGHT  20
+/* The width of the bar. Set to -1 to fit screen */
+#define BAR_WIDTH   -1
+/* Offset from the left. Set to 0 to have no effect */
+#define BAR_OFFSET 0
+/* Choose between an underline or an overline */
+#define BAR_UNDERLINE 1
+/* The thickness of the underline (in pixels). Set to 0 to disable. */
+#define BAR_UNDERLINE_HEIGHT 1
+/* Default bar position, overwritten by '-b' switch */
+#define BAR_BOTTOM 0
+/* The fonts used for the bar, comma separated. Only the first 2 will be used. */
+#define BAR_FONT "-*-stlarch-medium-r-*-*-10-*-*-*-*-*-*-*", "-*-terminus-medium-r-normal-*-12-*-*-*-c-*-*-1"
+//#define BAR_FONT "-*-nu-*-*-*-*-*-*-*-*-*-*-*-*","fixed"
+//#define BAR_FONT "-*-dejavu sans mono-medium-r-normal--*-80-*-*-*-*-iso10646-1","fixed"
+///#define BAR_FONT "-*-dejavu sans mono-*-*-*-*-*-*-*-*-*-*-*-*"
+//#define BAR_FONT "-*-dejavu sans mono-*-*-*-*-*-*-*-*-*-*-*-*"
+/* Some fonts don't set the right width for some chars, pheex it */
+#define BAR_FONT_FALLBACK_WIDTH 6
+/* Define the opacity of the bar (requires a compositor such as compton) */
+#define BAR_OPACITY 1.0 /* 0 is invisible, 1 is opaque */
+/* Color palette */
+#define BACKGROUND  0x151515
+#define FOREGROUND  0xd0d0d0
+#define COLOR0  0x151515
+#define COLOR1  0xd0d0d0
+#define COLOR2  0x90a959
+#define COLOR3  0xf4bf75
+#define COLOR4  0x6a9fb5
+#define COLOR5  0xaa759f
+#define COLOR6  0x75b5aa
+#define COLOR7  0x8f5536
+#define COLOR8  0x505050
+#define COLOR9  0xd28445
+
