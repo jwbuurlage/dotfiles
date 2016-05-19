05d3d72fa3ef0903b524235ca47cbce6ab5ba964
Jan-Wilem Buurlage <janwillembuurlage@gmail.com>
2016-05-19 10:04:56 +0200

    Add tmux config

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ cp vimrc ~/.vimrc
 cp Xresources ~/.Xresources
 cp gitconfig ~/.gitconfig
 cp bash_profile ~/.bash_profile
+cp tmux.conf ~/.tmux.conf
 
 cp -r ncmpcpp ~/.ncmpcpp
 cp -r config ~/.config
